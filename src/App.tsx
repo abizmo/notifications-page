9a73c9c750fcb032af7ffa6499171a4ad25a952c
Abián Izquierdo <abizmo@gmail.com>
2022-10-27 21:03:40 +0100

    Testing workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1>Notifications Page</h1>;
+  return (
+    <>
+      <h1>Notifications Page</h1>
+      <p>testing workflow</p>
+    </>
+  );
 }
 
 export default App;
