f77a88faea9a30ab1c71585055d3f7f2407fb721
Abián Izquierdo <abizmo@gmail.com>
2022-11-05 21:32:20 +0000

    Feat: Add Notification Component

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -13,6 +13,7 @@ import { UnreadDot } from '../UnreadDot';
 import style from './Notification.module.css';
 
 interface NotificationProps {
+  timeAgo: string;
   unread?: boolean;
   userName: string;
 }
@@ -23,6 +24,7 @@ const NotificationContext: Context<NotificationProps> = createContext(
 
 const Notification = ({
   children,
+  timeAgo,
   unread = false,
   userName,
 }: PropsWithChildren<NotificationProps>) => {
@@ -32,7 +34,7 @@ const Notification = ({
   if (unread) classnames.push(style.notificationUnread);
 
   return (
-    <NotificationContext.Provider value={{ unread, userName }}>
+    <NotificationContext.Provider value={{ timeAgo, unread, userName }}>
       <div className={classnames.join(' ')}>
         <div className={style.avatar}>
           <Image imageUrl={avatar} alt={userName} circle />
@@ -43,28 +45,60 @@ const Notification = ({
   );
 };
 
-Notification.Message = function NotificationMessage({
+// TODO: notificationGroup
+Notification.Group = function NotificationGroup({
   children,
 }: PropsWithChildren) {
-  return (
-    <>
-      <Notification.Body>sent you a private message</Notification.Body>
-      <Message>{children}</Message>
-    </>
-  );
+  return <div className={style.notificationGroup}>{children}</div>;
 };
 
-Notification.Body = function NotificationBody({ children }: PropsWithChildren) {
-  const { userName, unread } = useContext(NotificationContext);
+// TODO: notificationSubject
+Notification.Subject = function NotificationSubject({
+  children,
+}: PropsWithChildren) {
+  const { timeAgo, unread, userName } = useContext(NotificationContext);
   return (
     <div className={style.notificationSubject}>
       <p>
-        <span className={style.notificationUser}>{userName}</span> {children}{' '}
+        <span className={style.notificationUser}>{userName}</span> {children}
         <UnreadDot show={unread} />
       </p>
-      <p className={style.notificationTimeAgo}>1m ago</p>
+      <p className={style.notificationTimeAgo}>{timeAgo}</p>
     </div>
   );
+};
+
+// TODO: add class notificationLink
+Notification.Link = function NotificationLink({
+  children,
+  href,
+}: PropsWithChildren<{ href: string }>) {
+  return (
+    <a href={href} className={style.notificationLink}>
+      {children}
+    </a>
+  );
+};
+
+// TODO: notificationPicture
+Notification.Picture = function NotificationPicture({
+  alt,
+  src,
+}: {
+  alt: string;
+  src: string;
+}) {
+  return (
+    <a href='#' className={style.notificationPicture}>
+      <Image imageUrl={src} alt={alt} />
+    </a>
+  );
+};
+
+Notification.Message = function NotificationMessage({
+  children,
+}: PropsWithChildren) {
+  return <Message>{children}</Message>;
 };
 
 export default Notification;
