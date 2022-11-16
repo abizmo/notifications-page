fc12e4796793e86cc5369dd3e597ceebba2fd992
abizmo <abizmo@gmail.com>
2022-11-16 09:59:03 +0000

    Feat: Add NotificationsList Component

---
+++ src/components/NotificationsList/NotificationsList.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import type { Notification as NotificationType } from '../../assets/notifications';
+
+import Notification from '../Notification/Notification';
+
+const NotificationsList = ({
+  notifications,
+}: {
+  notifications: NotificationType[];
+}) => {
+  if (!notifications || notifications.length === 0)
+    return <p style={{ textAlign: 'center' }}>No notifications</p>;
+
+  const gridGapStyle = { '--gap': 'var(--sz-2)' } as React.CSSProperties;
+
+  return (
+    <ul className='grid' role='list' style={gridGapStyle}>
+      {notifications.map((notification) => (
+        <li key={notification.id}>
+          <Notification
+            date={notification.date}
+            user={notification.user}
+            unread={notification.unread}
+          >
+            <Notification.Group>
+              <Notification.Subject>
+                {notification.subject}
+                {!!notification.link && (
+                  <>
+                    {' '}
+                    <Notification.Link href={notification.link.url}>
+                      {notification.link.label}
+                    </Notification.Link>
+                  </>
+                )}
+              </Notification.Subject>
+              {!!notification.picture && (
+                <Notification.Picture
+                  alt={notification.picture.alt}
+                  src={notification.picture.src}
+                />
+              )}
+            </Notification.Group>
+            {!!notification.message && (
+              <Notification.Message>
+                {notification.message.text}
+              </Notification.Message>
+            )}
+          </Notification>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NotificationsList;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export * from './Header';
 export * from './Image';
 export * from './Message';
 export * from './Notification';
+export * from './NotificationsList';
 export * from './UnreadDot';
